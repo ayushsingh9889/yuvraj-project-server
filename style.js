4bce99691ae171c0699b9ad65593394e2be24bdf
Ayush singh <204816449+ayushsingh9889@users.noreply.github.com>
2026-06-16 10:24:31 +0530

    Delete style.js

--- style.js
+++
@@ -1,9 +0,0 @@
-let count = 0;
-
-const button = document.getElementById("counter-button");
-const display = document.getElementById("counter-display");
-
-button.addEventListener("click", () => {
-    count++;
-    display.textContent = `You clicked ${count} times`;
-});
